1bc45e891ef02ef60e3decde7ae368bff63b43b1
zoftrix <zofcos@yahoo.fr>
2021-03-08 17:15:37 +0100

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,4 @@
 <?php
 echo "Hello PHP!";
 ?>
+https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZTY1Y2NjMDgtMWE2My00ODY4LWJiNjQtMzk2NmMzMmFmMzBl%40thread.v2/0?context=%7b%22Tid%22%3a%22a1eaccf0-cf70-445d-8246-0a70aed83460%22%2c%22Oid%22%3a%2251638b0e-55a3-45e5-8b70-b52318eef924%22%7d
